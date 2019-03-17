8431da209d25ba76d9313c586f1fa4ca11824e36
Aaron <aaron.jense@protonmail.com>
2019-03-17 14:51:01 -0600

    Update licensing

--- matlab_verification/lzc.m
+++ matlab_verification/lzc.m
@@ -1,3 +1,25 @@
+%----------------------------------------------------------------------------
+%-- Description:  Find the leading Zeros from lzc_vector input.
+%-- This was created using auto-generated logic from a Matlab script
+%----------------------------------------------------------------------------
+%-- Author:  Ross K. Snider, Ph.D., Montana State University
+%-- Company: Montana State University
+%-- email address: rksnider@ece.montana.edu
+%-- Create Date:   March 20, 2014
+%----------------------------------------------------------------------------
+%-- Copyright (c) 2013 Ross K. Snider.
+%-- All rights reserved. Redistribution and use in source and binary forms
+%-- are permitted provided that the above copyright notice and this paragraph
+%-- are duplicated in all such forms and that any documentation, advertising
+%-- materials, and other materials related to such distribution and use
+%-- acknowledge that the software was developed by Montana State University
+%-- (MSU).  The name of MSU may not be used to endorse or promote products
+%-- derived from this software without specific prior written permission.
+%-- THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+%-- IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+%-- WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. 
+%----------------------------------------------------------------------------
+
 function lzc_count = lzc(lzc_vect)
 
 % Isolate each bit from lzc_vect
--- vhdl/lzc.vhd
+++ vhdl/lzc.vhd
@@ -1,3 +1,24 @@
+----------------------------------------------------------------------------
+-- Description:  Find the leading Zeros from lzc_vector input.
+-- This is auto-generated code from a Matlab script created by Ross K. Snider
+----------------------------------------------------------------------------
+-- Author:  Ross K. Snider, Ph.D., Montana State University
+-- Company: Montana State University
+-- email address: rksnider@ece.montana.edu
+-- Create Date:   March 20, 2014
+----------------------------------------------------------------------------
+-- Copyright (c) 2013 Ross K. Snider.
+-- All rights reserved. Redistribution and use in source and binary forms
+-- are permitted provided that the above copyright notice and this paragraph
+-- are duplicated in all such forms and that any documentation, advertising
+-- materials, and other materials related to such distribution and use
+-- acknowledge that the software was developed by Montana State University
+-- (MSU).  The name of MSU may not be used to endorse or promote products
+-- derived from this software without specific prior written permission.
+-- THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+-- IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+-- WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. 
+----------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
